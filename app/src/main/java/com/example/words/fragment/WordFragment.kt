5592395e976f2dc6ab5debf6c188f3e0ff05ddc2
lusiro <lusiro2001@students.amikom.ac.id>
2022-09-09 10:23:31 +0700

    update project 1.5 make toast in word when word on clicked

--- app/src/main/java/com/example/words/fragment/WordFragment.kt
+++ app/src/main/java/com/example/words/fragment/WordFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.words.R
@@ -49,7 +50,19 @@ class WordFragment : Fragment() {
         rvWords.layoutManager = lmW
 
         // set adapter into recyclerview
-        rvWords.adapter = wordAdapter
+        var adapter = wordAdapter
+        rvWords.adapter = adapter
+        adapter.setOnItemClickListener(object : WordAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+                val text = "You Clicked on Item $position"
+                val duration = Toast.LENGTH_SHORT
+                val aplicationContext = activity?.applicationContext
+
+                val toast = Toast.makeText(aplicationContext,text,duration)
+                toast.show()
+            }
+
+        })
 
         return view
     }
