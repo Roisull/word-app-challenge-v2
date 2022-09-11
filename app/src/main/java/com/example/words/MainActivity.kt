0bd4c2284e866ce21e1aae52d7a35a5b4b0095b0
lusiro <lusiro2001@students.amikom.ac.id>
2022-09-11 13:30:12 +0700

    word app v2 - change color all in values

--- app/src/main/java/com/example/words/MainActivity.kt
+++ app/src/main/java/com/example/words/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.words
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.words.fragment.DetailWordFragment
+import com.example.words.fragment.WordFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var wordFragment = WordFragment()
+    private var detailWordFragment = DetailWordFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // replace for first fragment = WordFragment.kt
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container_main, wordFragment).commit()
     }
 }
